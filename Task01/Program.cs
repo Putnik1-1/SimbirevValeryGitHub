2775ec29800924719b0b249403a7fefea2fe1dba
Valery Simbirev <valerkin2001@yandex.ru>
2023-02-10 21:45:08 +0300

    task01

---
+++ Task01/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b * b)
+{
+    Console.WriteLine("Первое число, квадрат  второго");
+}
+else
+{
+    Console.WriteLine("Первое число, не квадрат второго");
+}
