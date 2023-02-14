cee8e32afceddc432f649cd5ae18af44bf372d65
Valery Simbirev <valerkin2001@yandex.ru>
2023-02-14 22:15:46 +0300

    Task14

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -4,3 +4,18 @@
 // 14 -> нет
 // 46 -> нет
 // 161 -> да
+
+Console.WriteLine("Введи число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = 7;
+int num2 = 23;
+
+bool Multiplicity(int number)
+{
+    return num % num1 == 0 && num % num2 == 0;
+}
+
+if (Multiplicity(num))
+    Console.WriteLine($"Введенное число {num} кратно {num1} и {num2}");
+else Console.WriteLine($"Введенное число {num} не кратно {num1} и {num2}");
+
