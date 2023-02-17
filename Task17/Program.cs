2668402fbb3230526fdb0f11cc89e7b43da43733
Valery Simbirev <valerkin2001@yandex.ru>
2023-02-17 20:41:51 +0300

    Task18

--- Task17/Program.cs
+++ Task17/Program.cs
@@ -9,16 +9,16 @@ Console.Write("Введите координаты точки y: ");
 int yc = Convert.ToInt32(Console.ReadLine());
 
 int quater = Quarter(xc, yc);
-string result = quater > 0 
+string result = quater > 0
                 ? $"Указанные координаты соответсвуют четверти {quater}"
                 : "Введены некоректные координаты";
 Console.Write(result);
 
 int Quarter(int x, int y)
 {
-    if(x > 0 && y > 0) return 1;
-    if(x < 0 && y > 0) return 2;
-    if(x < 0 && y < 0) return 3;
-    if(x > 0 && y < 0) return 4;
+    if (x > 0 && y > 0) return 1;
+    if (x < 0 && y > 0) return 2;
+    if (x < 0 && y < 0) return 3;
+    if (x > 0 && y < 0) return 4;
     return 0;
 }
---
+++ Task18/Program.cs
@@ -0,0 +1,19 @@
+﻿//  18: Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+Console.Write("Введите число от 1 до 4: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string quater = Quarter(number);
+
+Console.Write($"Диапазон координат {quater}");
+
+string Quarter(int numberA)
+{
+    if (numberA == 1) return "x > 0 && y > 0";
+    if (numberA == 2) return "x < 0 && y > 0";
+    if (numberA == 3) return "x < 0 && y < 0";
+    if (numberA == 4) return "x > 0 && y < 0";
+    return "не соответсвует номеру четверти";
+}
