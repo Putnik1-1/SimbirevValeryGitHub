d56b5828e3017f2d9ee55260e24af1a3685d731f
Valery Simbirev <valerkin2001@yandex.ru>
2023-02-23 19:41:54 +0300

    Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, 
+// которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 9999 && number < 100000)
+{
+    if(Polydrome(number)) Console.Write($"Данное число: {number} полиндром");
+    else Console.Write($"Данное число: {number} не полиндром");
+}
+else
+{
+    Console.Write("Вы ввели не пятизначное число");
+}
+
+bool result = Polydrome(number);
+
+bool Polydrome(int num)
+{
+    int num1 = num % 10;
+    int num2 = num / 10 % 10;
+    int num3 = num / 100 % 10;
+    int num4 = num / 1000 % 10;
+    int num5 = num / 10000;
+    int newNumber = num1 * 10000 + num2 * 1000 + num3 * 100 + num4 * 10 + num5;
+    if(num1 == num5 && num2 == num4) return true;
+    else return false;
+}
--- Task22/Program.cs
+++ Task22/Program.cs
@@ -12,9 +12,9 @@ Square(number);
 void Square(int num)
 {
     int index = 1;
-    while (index <= num) 
+    while (index <= num)
     {
-        Console.WriteLine($"{index, 5} - {index * index, 5}");
+        Console.WriteLine($"{index,5} - {index * index,5}");
         index++;
     }
 }
