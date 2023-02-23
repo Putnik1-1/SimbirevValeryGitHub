abb27a7505ef141c4e98b78454f1731a1b66ab5c
Valery Simbirev <valerkin2001@yandex.ru>
2023-02-23 20:03:13 +0300

    Task19.2

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -5,28 +5,46 @@
 // 12821 -> да
 // 23432 -> да
 
-Console.WriteLine("Введите пятизначное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-if (number > 9999 && number < 100000)
-{
-    if(Polydrome(number)) Console.Write($"Данное число: {number} полиндром");
-    else Console.Write($"Данное число: {number} не полиндром");
-}
-else
-{
-    Console.Write("Вы ввели не пятизначное число");
-}
+// Console.WriteLine("Введите пятизначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number > 9999 && number < 100000)
+// {
+//     if(Polydrome(number)) Console.Write($"Данное число: {number} полиндром");
+//     else Console.Write($"Данное число: {number} не полиндром");
+// }
+// else
+// {
+//     Console.Write("Вы ввели не пятизначное число");
+// }
 
-bool result = Polydrome(number);
+// bool Polydrome(int num)
+// {
+//     int num1 = num % 10;
+//     int num2 = num / 10 % 10;
+//     int num3 = num / 100 % 10;
+//     int num4 = num / 1000 % 10;
+//     int num5 = num / 10000;
+//     int newNumber = num1 * 10000 + num2 * 1000 + num3 * 100 + num4 * 10 + num5;
+//     if(num1 == num5 && num2 == num4) return true;
+//     else return false;
+// }
+
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(Polydrome(number)) Console.Write($"Данное число: {number} полиндром");
+else Console.Write($"Данное число: {number} не полиндром");
 
 bool Polydrome(int num)
 {
-    int num1 = num % 10;
-    int num2 = num / 10 % 10;
-    int num3 = num / 100 % 10;
-    int num4 = num / 1000 % 10;
-    int num5 = num / 10000;
-    int newNumber = num1 * 10000 + num2 * 1000 + num3 * 100 + num4 * 10 + num5;
-    if(num1 == num5 && num2 == num4) return true;
+    int numA = 0;
+    int newNumber = num;
+    while (num > 0)
+    {
+        numA = numA * 10 + num % 10;// num= numA = 3 numA = 32 numA = 321
+        num = num / 10;
+    }
+    if(numA == newNumber) return true;
     else return false;
 }
