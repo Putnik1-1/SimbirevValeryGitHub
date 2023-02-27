57e5434de5b9471dece78756507f44f75556d968
Valery Simbirev <valerkin2001@yandex.ru>
2023-02-27 16:42:38 +0300

    Task24

---
+++ Task24/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sum = SumNumbers(number);
+Console.Write($"Сумма чисел от 1 до {number} равна {sum}");
+
+int SumNumbers(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
