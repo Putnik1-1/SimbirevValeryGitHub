c642d376d7cca247ec4f31114c9ed751dfdb4906
Valery Simbirev <valerkin2001@yandex.ru>
2023-02-27 21:53:13 +0300

    Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, 
+// которая принимает на вход число и 
+// выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int len = NumberLen(number);
+SumNumbers(number, len);
+
+int NumberLen(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+void SumNumbers(int num, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    Console.WriteLine(sum);
+}
