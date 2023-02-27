9dc679319a55c74a89e02391c31198a8ef6ba44c
Valery Simbirev <valerkin2001@yandex.ru>
2023-02-27 21:57:37 +0300

    Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите длинну массива: ");
+int array = Convert.ToInt32(Console.ReadLine());
+
+int[] randomArray = new int[array];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1,9);
+    Console.Write(randomArray[i] + " ");
+}
