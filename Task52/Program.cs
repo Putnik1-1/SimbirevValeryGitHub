acf5b9c6f136a86d9b39e569fe64583c1873206e
Valery Simbirev <valerkin2001@yandex.ru>
2023-03-13 15:26:26 +0300

    Task54

--- Task52/Program.cs
+++ Task52/Program.cs
@@ -57,7 +57,7 @@ void PrintArray(double[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write(array[i] + "; "));
+        Console.Write(array[i] + "; ");
     }
     Console.WriteLine();
 }
